1060111130ed747d4a36725e1b93adf95fb01fbf
chris <christopher@burrows.me>
2022-12-19 17:33:05 +0000

    Removing useless comments

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -8,7 +5,6 @@ import (
 	"github.com/verminio/datastore-cmd/namespace"
 )
 
-// listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List namespaces",
@@ -25,14 +21,4 @@ var listCmd = &cobra.Command{
 
 func init() {
 	namespaceCmd.AddCommand(listCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
